17f6ba0af6053dca84f1ecf300b13e348ec3d8db
tcoleman5 <47992464+tcoleman5@users.noreply.github.com>
2020-12-09 02:37:24 -0600

    Create Inventory.java

---
+++ Inventory.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Inventory {
+    private String products;
+    private double prices;
+
+    public Inventory(String name, double price) {
+        this.products = products;
+        this.prices = prices;
+    }
+    public Inventory() {
+    }
+
+    public String getProducts() {
+        return products;
+    }
+
+    public void setName(String products) {
+        this.inventory = inventory;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrices(String products) {
+        this.prices = prices;
